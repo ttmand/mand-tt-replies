30ab2a0ecd4c2107aab072f98331c9f4aeb43bd9
ttmand <dmacleod0035+ttmand@gmail.com>
2026-05-19 14:53:41 +0100

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+472815
+I would recommend you don't click on any unknown links. It's important to be vigilant in situations like this.
+
+Some things to consider:
+
+- If you didn't ask for links
+- If it is a random person sending them
+- If you don't recognise the website URL
+
+I would say these things would make it more likely to be suspicious. Of course, all these things could apply and it could still be safe; likewise, none could apply and it could be dangerous.
+19/5/2026
+
+472767
+Spam is usually deleted when a moderator sees it. It may not happen immediately, depending on whether there are moderators online at the time.
+15/5/2026
+
